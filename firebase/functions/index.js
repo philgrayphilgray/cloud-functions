692bc9b5c204c8c62572162e9d90c4af9df06ee3
philgrayphilgray <thephilgray@gmail.com>
2018-04-05 15:12:04 -0400

    Apply last stash.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,104 @@
 const functions = require('firebase-functions');
+const os = require('os');
+const path = require('path');
+const spawn = require('child-process-promise').spawn;
+const cors = require('cors')({ origin: true });
+const Busboy = require('busboy');
+const fs = require('fs');
 
+const gcconfig = {
+  projectId: 'fb-cloud-functions-e49d0',
+  keyFilename:
+    'fb-cloud-functions-e49d0-firebase-adminsdk-du6a0-7dd9e337d5.json'
+};
+
+const gcs = require('@google-cloud/storage')(gcconfig);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+// exports.onFileChange = functions.storage.object().onChange(event => {
+//   const object = event.data;
+//   const bucket = object.bucket;
+//   const contentType = object.contentType;
+//   const filePath = object.name;
+//   console.log('File change detected, function execution started');
+
+//   if (object.resourceState === 'not_exists') {
+//     console.log('We deleted a file, exit...');
+//     return;
+//   }
+
+//   if (path.basename(filePath).startsWith('resized-')) {
+//     console.log('We already renamed that file!');
+//     return;
+//   }
+
+//   const destBucket = gcs.bucket(bucket);
+//   const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
+//   const metadata = { contentType: contentType };
+//   return destBucket
+//     .file(filePath)
+//     .download({
+//       destination: tmpFilePath
+//     })
+//     .then(() => {
+//       return spawn('convert', [tmpFilePath, '-resize', '300x300', tmpFilePath]);
+//     })
+//     .then(() => {
+//       return destBucket.upload(tmpFilePath, {
+//         destination: 'resized-' + path.basename(filePath),
+//         metadata: metadata
+//       });
+//     });
 // });
+
+exports.uploadFile = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (req.method !== 'POST') {
+      return res.status(500).json({
+        message: 'Not allowed'
+      });
+    }
+    const busboy = new Busboy({ headers: req.headers });
+    let uploadData = null;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      const filepath = path.join(os.tmpdir(), filename);
+      uploadData = { file: filepath, type: mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on('finish', () => {
+      const bucket = gcs.bucket('fb-cloud-functions-e49d0.appspot.com');
+      bucket
+        .upload(uploadData.file, {
+          uploadType: 'media',
+          metadata: {
+            metadata: {
+              contentType: uploadData.type
+            }
+          }
+        })
+        .then(response => {
+          let uploadedFile = response[0];
+          return uploadedFile.getSignedUrl({
+            action: 'read',
+            expires: '03-17-2025'
+          });
+        })
+        .then(url => {
+          res.status(200).json({
+            url,
+            message: 'It worked!'
+          });
+        })
+
+        .catch(err => {
+          res.status(500).json({
+            error: err
+          });
+        });
+    });
+    busboy.end(req.rawBody);
+  });
+});
